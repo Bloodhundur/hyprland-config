7b9015641c5ce5b37f29ea60414b67c5d9bf178a
Bloodhundur <shahyan.soltani@gmail.com>
2025-06-20 13:13:55 -0400

    greater support for my laptop

--- hypr/.config/hypr/bin/display-toggle.sh
+++ hypr/.config/hypr/bin/display-toggle.sh
@@ -1,12 +1,17 @@
 
-MONITOR="DP-3"
+# MONITOR="DP-3"
+# MONITORLAPTOP="eDP-1"
+
+MONITOR=$(hyprctl monitors | grep '^Monitor ' | awk '{print $2}')
 
 CURRENT_RES=$(hyprctl monitors| grep -o -m 1 '[0-9]\+x[0-9]\+' | head -1)
 
-if [[ "$CURRENT_RES" == "5120x1440" ]]; then
+if [[ "$MONITOR" == "eDP-1" ]]; then
+	hyprctl keyword monitor "$MONITOR, 1920x1080@144.00, 0x0, 1"
+
+elif [[ "$CURRENT_RES" == "5120x1440" ]]; then
  hyprctl keyword monitor "$MONITOR, 2560x1440@240.00, 0x0, 1, bitdepth, 10"
 else
   hyprctl keyword monitor "$MONITOR, 5120x1440@240.00, 0x0, 1, bitdepth, 10"
 fi
-echo "$CURRENT_RES"
-
+# $CURRENT_RES"
