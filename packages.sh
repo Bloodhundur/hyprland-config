e005004d84033fc5e76ec2458a56ce3d4711985f
shay <shahyan.soltani@gmail.com>
2026-05-08 18:50:38 -0400

    made cooler

---
+++ packages.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# a list of mandatory packages to install on a fresh system for config files to work
+
+# basic first lines of interfaces
+sudo pacman -Sy hyprlock waybar kitty swaync hyprpaper hyprlock rofi quickshell
+
+# important audio and display stuff
+sudo pacman -Sy brightnessctl pipewire pipewire-alsa libpipewire libwireplumber pipewire-audio pipewire-jack pipewire-pulse pipewire-session-manager wireplumber pavucontrol
+
+# tools
+sudo pacman -Sy firefox slurp wl-clipboard
+
+# performance 
+sudo pacman -Sy power-profiles-daemon gobject-introspection python-gobject 
+
+# coding related
+sudo pacman -Sy rust
